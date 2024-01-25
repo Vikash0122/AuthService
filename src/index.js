b1dff30042d9e1e6997f96c85c9ae20549f92a81
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-01-25 10:14:41 +0530

    added role to user in user_roles table

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { PORT } = require("./config/serverConfig");
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/index');
 // const db = require("./models/index");
+// const { User, Role } = require("./models/index");
 
 
 const getServerStarted = async() => {
@@ -13,6 +14,9 @@ const getServerStarted = async() => {
 
   app.listen(PORT, async() => {
 //    db.sequelize.sync({ alter: true });
+      // const u1 = await User.findByPk(3);
+      // const r1 = await Role.findByPk(2);
+      // u1.addRole(r1);
     console.log(`Server Started on Port: ${PORT}`);
   });
 };
